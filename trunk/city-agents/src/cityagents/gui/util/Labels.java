362b01c2ef310630c17e979526430c970b7785df
spa.simone <spa.simone@054875bd-76b1-b842-3dcb-c29a563117cf>
2011-05-10 08:06:24 +0000

    Finalized the editor that permits to edit the directions. Added the class "Label" that provide constants for the GUI label. This solution substitutes other error-prone solutions. Added the library for the graphs.

---
+++ trunk/city-agents/src/cityagents/gui/util/Labels.java
@@ -0,0 +1,76 @@
+/**
+ * City Agents is a framework for intelligent mobile agents.
+ * Copyright (C) 2011 Deep Blue Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package cityagents.gui.util;
+
+/**
+ *
+ * @author Deep Blue Team
+ */
+public interface Labels {
+
+	/* *** EditDirectionContextMenu.java *** */
+	
+	public static final String NORTH_MENU = "North..";
+	public static final String SOUTH_MENU = "South..";
+	public static final String WEST_MENU = "West..";
+	public static final String EAST_MENU = "East..";
+	
+	public static final String NORTH = "North";
+	public static final String SOUTH = "South";
+	public static final String WEST = "West";
+	public static final String EAST = "East";
+	
+	public static final String NORTH_WEST = "North-West";
+	public static final String NORTH_EAST = "North-East";
+	public static final String SOUTH_WEST = "South-West";
+	public static final String SOUTH_EAST = "South-East";
+	public static final String WEST_EAST = "West-East";
+	public static final String NORTH_SOUTH = "North-South";
+	
+	public static final String NORTH_WEST_EAST = "North-West-East";
+	public static final String SOUTH_WEST_EAST = "South-West-East";
+	public static final String WEST_NORTH_SOUTH = "West-North-South";
+	public static final String EAST_NORTH_SOUTH = "East-North-South";
+	
+	public static final String CLOSE = "Close editing";
+	
+	/* *** ContextMenu.java *** */
+	
+	public static final String EDIT_DIRECTIONS = "Edit directions";
+	
+	/* *** CaMenuBar.java *** */
+	
+	public static final String FILE = "File";
+	public static final String LOAD_MAP = "Load map";
+	public static final String STORE_MAP = "Store map";
+	public static final String EXIT = "Exit";
+	
+	public static final String WORLD = "World";
+	public static final String INCREASE_DIMENSION = "Increase dimension";
+	public static final String DECREASE_DIMENSION = "Decrease dimension";
+	
+	public static final String AGENT = "Agent";
+	public static final String ADD_NEW = "Add new";
+	public static final String ADD_RANDOM = "Add random";
+	
+	public static final String HELP = "Help";
+	public static final String ABOUT = "About City Agents";
+	
+	/* *** *** */
+	
+}
